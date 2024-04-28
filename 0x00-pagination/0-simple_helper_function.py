d34b6700974255045a6e25bc3c5355dcefef9985
Abdul Rahman <abdulnsamba@gmail.com>
2024-04-28 08:01:40 +0300

    func doc

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,7 @@ function to return a tuple with start and end index
 
 
 def index_range(page, page_size):
+    """ returns the start and end indices"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
