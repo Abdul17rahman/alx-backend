cd762c81c0622fe1a3c61d18c918203db96817b7
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-28 16:12:14 +0300

    csv

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+function to return a tuple with start and end index
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """ returns the start and end indices"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
