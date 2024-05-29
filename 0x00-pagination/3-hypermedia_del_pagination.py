e1b0801ec521f38cc8c396740d3f88f1dbc46879
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-29 09:58:38 +0300

    fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-function to return a tuple with start and end index
+Deletion-resilient hypermedia pagination
 """
 
 import csv
@@ -9,16 +9,16 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
