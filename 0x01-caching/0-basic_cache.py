8b2550f87448cb35987f114b5b9a8bfd74013885
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-29 15:09:29 +0300

    implement put and get

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,10 +14,19 @@ class BasicCache(BaseCaching):
         """
         This is intializing functionality from parent class
         """
-        super()
+        super().__init__()
 
     def put(self, key, item):
-        pass
+        """
+        Adds an item to the cache dict
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        pass
+        """
+        Retrives the item
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
