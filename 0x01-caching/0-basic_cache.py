5198298c1834a33ff7ee7c42ef57d74e484af3ce
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-29 12:55:12 +0300

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Basic cache class
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """
+        This is intializing functionality from parent class
+        """
+        super()
+
+    def put(self, key, item):
+        pass
+
+    def get(self, key):
+        pass
---
+++ 0x01-caching/README.md
@@ -0,0 +1,2 @@
+# 0x01-caching
+Caching is a way of storing data on a short term.
