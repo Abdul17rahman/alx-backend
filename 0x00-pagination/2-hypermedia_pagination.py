24527e502691ae746016ed94e476428ed10fc64f
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-29 09:41:16 +0300

    added hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -46,3 +46,18 @@ class Server:
         if start_index >= len(dataset):
             return []
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Returns a dictionary with hypermedia pagination information."""
+        data = self.get_page(page, page_size)
+        dataset_length = len(self.dataset())
+        total_pages = math.ceil(dataset_length / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
