c14c646b83bc834ee4c710a893781c68d9b46394
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-30 10:28:55 +0300

    test mru

--- 0x01-caching/mru_copy.py
+++ 0x01-caching/mru_copy.py
@@ -4,26 +4,30 @@ MRUCache module
 """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
     """ MRUCache implements:
-      - the put and get methods as last-in first-out
+      - the put and get methods as most recently used
     """
     def __init__(self):
         """
         This is intializing functionality from parent class
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
         Adds an item to the cache dict
         """
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                itm, _ = self.cache_data.popitem()
-                print("DISCARD: {}".format(itm))
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru, _ = self.cache_data.popitem(last=True)
+                print("DISCARD: {}".format(mru))
             self.cache_data[key] = item
 
     def get(self, key):
