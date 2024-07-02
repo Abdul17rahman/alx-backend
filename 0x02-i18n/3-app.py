65ed946c3a69f88b23a0b5c329c102b0cd57c7ff
Abdul Rahman <abdulnsamba@gmail.com>
2024-07-02 11:30:09 +0300

    msg

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic flask app module"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__, template_folder="./templates")
+babel = Babel(app)
+
+
+class Config:
+    """ This class configures the lang and tz"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets a default location"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ Returns hello world"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
